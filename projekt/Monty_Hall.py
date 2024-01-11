b90f63583b6d9f8cfb98d48d3443b4546353cc94
caspermalm <ecasper.malm@gmail.com>
2024-01-11 13:57:11 +0100

    klnkmlkn

--- projekt/Monty_Hall.py
+++ projekt/Monty_Hall.py
@@ -1,6 +1,9 @@
 import random
 
-# 0 = get, 1 = bil
+# 0 = goat, 1 = car
 doors=[0, 0, 0]
 loop = True
-while loop == True
+while loop == True:
+    doors[random.randint(0, 2)] = 1
+    answer = input("Which door do you choose? ")
+    
