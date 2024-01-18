c9378f6c8c6d2f68926748b54eed65e40ef40ffa
caspermalm <ecasper.malm@gmail.com>
2024-01-18 10:52:09 +0100

    gjort färdigt frågan

--- projekt/Monty_Hall.py
+++ projekt/Monty_Hall.py
@@ -5,5 +5,12 @@ doors=[0, 0, 0]
 loop = True
 while loop == True:
     doors[random.randint(0, 2)] = 1
-    answer = input("Which door do you choose? ")
+    anwser_loop = True
+    while anwser_loop == True:
+        answer = input("Which door do you choose? (1, 2, 3) ")
+        if answer == "1" or answer == "2" or answer == "3":
+            answer = int(answer)
+            answer = answer -1
+            anwser_loop = False 
+    
     
