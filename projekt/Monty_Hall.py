4e75d4042fd85f87aba1adbd71a815e43e7e1819
caspermalm <ecasper.malm@gmail.com>
2024-04-18 14:02:32 +0200

    jobbat med projekt

--- projekt/Monty_Hall.py
+++ projekt/Monty_Hall.py
@@ -2,20 +2,27 @@ import random
 
 # 0 = goat, 1 = car
 doors=[0, 0, 0]
-loop = True
+loop = input("?: ")
 while loop == True:
     doors[random.randint(0, 2)] = 1
+    print(doors)
     anwser_loop = True
+    
+    
     while anwser_loop == True:
-        answer = input("Which door do you choose? (1, 2, 3) ")
+        answer = int(input("Which door do you choose? (1, 2, 3) "))
         if answer == "1" or answer == "2" or answer == "3":
             answer = int(answer)
             answer = answer - 1
             anwser_loop = False 
-        found_goat_door = False
+        found_goat_door = False    
         for i in range(3): 
             if doors[i] == 0 and i != answer and found_goat_door == False:
-                print(i + 1)
+                print("door number", i + 1, "is wrong, now choose again")
                 found_goat_door = True
+        loop == False
 
+print("Rätt dörr är: ", doors.index(1) + 1)
     
+
+
