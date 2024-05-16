2b598dbc6d218757fb2f762e13d06a67f66f4c86
caspermalm <ecasper.malm@gmail.com>
2024-05-16 09:35:40 +0200

    fixat spela igen frågan, fixat så den väljer random när den säger vilken som är fel mm.

--- projekt/Monty_Hall.py
+++ projekt/Monty_Hall.py
@@ -7,17 +7,31 @@ answer = int(input("Which door do you choose? (1, 2, 3) "))
 if answer == 1 or answer == 2 or answer == 3:
     print(doors)
     
-    
+potential_dors_to_open = []   
 if answer == 1 or answer == 2 or answer == 3:
     answer = int(answer)
     answer = answer - 1
 for i in range(3): 
     if doors[i] == 0 and i != answer:
-        print("door number", i + 1, "is wrong, now choose again")
+        potential_dors_to_open.append(i)
+index = random.randint(0,(len(potential_dors_to_open) -1))
+print("door number", potential_dors_to_open[index] + 1, "is wrong, now choose again")
+print(potential_dors_to_open ,index)
 switch_door = input("Do you want to switch door? ")
 if switch_door == "yes":
     new_answer = int(input("Which door do you choose? (1, 2, 3) "))
     new_answer = new_answer - 1
+    if doors[new_answer] == 1:
+        print("You won! ")
+    else:
+        print("You lost :( ")
+else:
+    if doors[answer] == 1:
+        print("You won! ")
+    else:
+        print("You lost :( ")
+
+
 #print("Rätt dörr är: ", doors.index(1) + 1)
     
 
